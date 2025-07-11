373f1172771e6bd28c42c432dfc83534f5785091
Emmanuel olu jimoh <olujimohibukunemmanuel@gmail.com>
2025-07-10 17:57:38 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('toggle-theme').addEventListener('click', () => {
+  const body = document.body;
+  if (body.classList.contains('light-mode')) {
+    body.classList.remove('light-mode');
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+    body.classList.add('light-mode');
+  }
+});
