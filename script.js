4ba1f63026ec7feeec66e16157b06e2ae0516b67
Emmanuel olu jimoh <olujimohibukunemmanuel@gmail.com>
2025-07-11 13:20:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,13 @@ input.addEventListener('keydown', (e) => {
     }
   }
 });
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  if (menuToggle && mobileMenu) {
+    menuToggle.addEventListener('click', () => {
+      mobileMenu.classList.toggle('show');
+    });
+  }
+});
